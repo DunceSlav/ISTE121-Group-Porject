26319e29bb3e8c23e8605a10ba3b4624dfa65e67
DunceSlav <90402689+DunceSlav@users.noreply.github.com>
2021-11-21 20:30:18 -0500

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -28,9 +28,53 @@ public class TCPClient extends Application implements EventHandler<ActionEvent> 
    private TextField tfServerIP = new TextField();
    private Button btnConnect = new Button("Connect");
    
+   //send button
+   private Button btnSend = new Button("Send Patient Info");
+   
    // Components - BOTTOM
    private Label lblLog = new Label("Log:");
    private TextArea taLog = new TextArea();
+   
+   //first name
+   private Label lblFname = new Label("First name:");
+   private TextField tfFname = new TextField();
+   //last name
+   private Label lblLname = new Label("Last name:");
+   private TextField tfLname = new TextField();
+   //age
+   private Label lblAge = new Label("Age:");
+   private TextField tfAge = new TextField();
+   //date of birth info
+   private Label lblDOBinfo = new Label("Date of birth mm/dd/yyyy:");
+   private TextField tfDOBmonth = new TextField();
+   private Label lblDOBday = new Label("/");
+   private TextField tfDOBday = new TextField();
+   private Label lblDOByear = new Label("/");
+   private TextField tfDOByear = new TextField();
+   //height
+   private Label lblHeight = new Label("Height:");
+   private TextField tfHeight = new TextField();
+   //weight
+   private Label lblWeight = new Label("Weight:");
+   private TextField tfWeight = new TextField();
+   
+   //reason for stay
+   private Label lblReason = new Label("Reason for stay:");
+   private RadioButton reason1 = new RadioButton("Reason 1");
+   private RadioButton reason2 = new RadioButton("Reason 2");
+   private RadioButton reason3 = new RadioButton("Reason 3");
+   private RadioButton reason4 = new RadioButton("Reason 4");
+   private RadioButton reason5 = new RadioButton("Reason 5");
+   private RadioButton reason6 = new RadioButton("Reason 6");
+   private RadioButton reason7 = new RadioButton("Reason 7");
+   private RadioButton reason8 = new RadioButton("Reason 8");
+   private RadioButton reason9 = new RadioButton("Reason 9");
+   private RadioButton reason10 = new RadioButton("Reason 10");
+   
+   //insurance y/n?
+   private Label lblInsurance = new Label("Does patient have insurance?");
+   private RadioButton radioY = new RadioButton("Yes");
+   private RadioButton radioN = new RadioButton("No");
    
    // networking attributes
    public static final int SERVER_PORT = 32001;
@@ -55,10 +99,6 @@ public class TCPClient extends Application implements EventHandler<ActionEvent> 
       stage.setResizable(false);
       root = new VBox(8);
       
-      // ina row 
-      
-      private Label lblSentence
-      
       // TOP - label, text field, button
       FlowPane fpTop = new FlowPane(8,8);
       fpTop.setAlignment(Pos.CENTER);
@@ -74,11 +114,41 @@ public class TCPClient extends Application implements EventHandler<ActionEvent> 
       fpBot.getChildren().addAll(lblLog, taLog);
       root.getChildren().add(fpBot);
       
+      //Patient Info section
+      GridPane infoSection = new GridPane();
+      //patient name
+      HBox nameBox = new HBox(8);
+      nameBox.getChildren().addAll(lblFname, tfFname, lblLname, tfLname);
+      infoSection.addRow(1, nameBox);
+      //patient age
+      HBox ageBox = new HBox(8);
+      ageBox.getChildren().addAll(lblAge, tfAge);
+      infoSection.addRow(2, ageBox);
+      //patient DOB
+      HBox dobBox = new HBox(8);
+      dobBox.getChildren().addAll(lblDOBinfo, tfDOBmonth, lblDOBday, tfDOBday, lblDOByear, tfDOByear);
+      infoSection.addRow(3,dobBox);
+      //patient height and weight
+      HBox hwBox = new HBox(8);
+      hwBox.getChildren().addAll(lblHeight, tfHeight, lblWeight, tfWeight);
+      infoSection.addRow(4, hwBox);
+      //patient reason for stay
+      VBox reasonBox = new VBox(8);
+      reasonBox.getChildren().addAll(lblReason, reason1, reason2, reason3, reason4, reason5, reason6, reason7, reason8, reason9, reason10);
+      infoSection.addRow(5, reasonBox);
+      //patient has insurance y/n?
+      VBox insuranceBox = new VBox(8);
+      insuranceBox.getChildren().addAll(lblInsurance, radioY, radioN);
+      infoSection.addRow(6, insuranceBox);
+      
+      
+      root.getChildren().add(infoSection);
+      
       // Listen for the button
       btnConnect.setOnAction(this);
 
       // Show window
-      scene = new Scene(root, 500, 250);
+      scene = new Scene(root, 700, 650);
       stage.setScene(scene);
       stage.show();      
    }
