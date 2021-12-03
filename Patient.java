a0bbda69085a4a61a01de471c7b2a831bbb0dea9
18androl <94425763+18androl@users.noreply.github.com>
2021-12-03 18:29:33 -0500

    Add files via upload

--- Patient.java
+++ Patient.java
@@ -1,5 +1,5 @@
 import java.io.Serializable;
-public class Patient implements Serializable
+public class Patient implements Serializable, Comparable<Patient>
 {
    // attributes
    private static final long serialVersionUID = 01L;
@@ -14,13 +14,13 @@ public class Patient implements Serializable
    private double weight;
    
    private String reason;
-   private double cost;
+   private int daysStayed;
    private boolean insurance;
-   // add insurance attribute later later
+   private double cost;
    
    
    // Constructor
-   public Patient(String f, String l, String d, int a, double h, double w, String r, double c)
+   public Patient(String f, String l, String d, int a, double h, double w, String r, int ds, double c, boolean i)
    {
       firstName = f;
       lastName = l;
@@ -30,6 +30,8 @@ public class Patient implements Serializable
       weight = w;
       reason = r;
       cost = c;
+      daysStayed = ds;
+      insurance = i;
    }
    
    
@@ -73,6 +75,17 @@ public class Patient implements Serializable
    {
       return cost;
    }
+   
+   public int getDays()
+   {
+      return daysStayed;
+   } 
+   
+   public boolean getInsurance()
+   {
+      return insurance;
+   }
+
    
    
    // Mutators
@@ -120,7 +133,24 @@ public class Patient implements Serializable
    {
       insurance = x;
    }
+   
+   public void setDays(int x)
+   {
+      daysStayed = x;
+   }
+   
+   
+   
+   // toString
+   public String toString() 
+   {
+      return (lastName + ", " + firstName + "   " + dateOfBirth + "   " + reason + "   " + cost);
+   }
 
 
-
+   @Override
+   public int compareTo(Patient p) {
+      return this.getLastName().compareTo(p.getLastName());
+   }
 }
+
--- TCPServer.java
+++ TCPServer.java
@@ -7,7 +7,7 @@ import javafx.scene.text.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 import javafx.geometry.*;
-
+import java.util.*;
 import java.net.*;
 import java.io.*;
 
@@ -17,7 +17,8 @@ import java.io.*;
  * @version 2205
  */
 
-public class TCPServer extends Application {
+public class TCPServer extends Application implements EventHandler<ActionEvent> 
+{
    // Window attributes
    private Stage stage;
    private Scene scene;
@@ -25,17 +26,27 @@ public class TCPServer extends Application {
    
    // GUI Components
    public TextArea taLog = new TextArea();
-   private Button btnFormat = new Button("PDF Download");
+   private Button btnFormat = new Button("Format");
    
 
    // Socket stuff
    private ServerSocket sSocket = null;
    public static final int SERVER_PORT = 32001;
    
+   //takes input from client and inputs it into an array list
+   private ArrayList<Patient> newList = new ArrayList<Patient>();
+   
+   //tree for organizing the patients by last name 
+   private TreeMap<String, Patient> tree_map = new TreeMap<String, Patient>();
+
+   // Patient recieved list
+   private List<Patient> patientList = new ArrayList<>();
+   
    /**
     * main program
     */
    public static void main(String[] args) {
+
       launch(args);
    }
    
@@ -47,9 +58,10 @@ public class TCPServer extends Application {
       // Window setup
       stage = _stage;
       stage.setTitle("Electronic Medical Records");
-      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-         public void handle(WindowEvent evt) { System.exit(0); }
-      });
+      stage.setOnCloseRequest(
+         new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent evt) { System.exit(0); }
+         });
       stage.setResizable(false);
       root = new VBox(8);
    
@@ -62,17 +74,31 @@ public class TCPServer extends Application {
       root.getChildren().add(fpBot);
       root.getChildren().add(btnFormat);
       
+      btnFormat.setOnAction(this);
+      
       // Show window
       scene = new Scene(root, 500, 400);
       stage.setScene(scene);
       stage.show();      
       
       // Do server work in a thread
-      Thread t = new Thread() {
-         public void run() { doServerWork(); }
-      };
+      Thread t = 
+         new Thread() {
+            public void run() { doServerWork(); }
+         };
       t.start();
    }
+   
+   public void handle(ActionEvent ae) {
+      String label = ((Button)ae.getSource()).getText();
+      switch(label) {
+ 
+         case "Format":
+            doFormat();
+            break;
+      }
+   }
+
    
    /** 
     * doServerWork
@@ -87,7 +113,7 @@ public class TCPServer extends Application {
          taLog.appendText("IO Exception (1): " + ioe);
          return;
       }
-
+   
       // Socket for communication with client      
       Socket cSocket = null;
       try {
@@ -101,5 +127,74 @@ public class TCPServer extends Application {
       
       // No real processing yet
       taLog.appendText("Client connected!\n");
-   }   
+      
+      
+      try
+      {
+         OutputStream outputStream = cSocket.getOutputStream();
+         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+      
+         InputStream inputStream = cSocket.getInputStream();
+         ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+         patientList = (ArrayList<Patient>)objectInputStream.readObject();
+      
+         taLog.appendText("Info received!\n");
+         
+         for(Patient p: patientList)
+         {       
+            tree_map.put(p.getLastName(), p);
+            taLog.appendText("Patient Received: " + p.toString());
+         }    
+        
+      }
+      
+      catch(Exception e)
+      {
+      
+      }
+   }  
+
+   public void doFormat()
+   {
+      String str = "";
+      if(!taLog.getText().isBlank() && !patientList.isEmpty())
+         str+="Info received!\n\n";
+
+      Collections.sort(patientList);
+      for(Patient p: patientList)
+      {
+         str+="Patient Received: "+p.toString()+"\n";
+      }
+      taLog.setText(str);
+      storeInFile(str);
+      encriptToFile();
+   
+   }
+   private void storeInFile(String str){
+      System.out.println("Storing method");
+      System.out.println(str);
+      try {
+         FileWriter fileWriter = new FileWriter(new File("src/main/resources/store.txt"));
+         fileWriter.write(str);
+         System.out.println("Writing");
+         fileWriter.close();
+      } catch (IOException e) {
+         System.out.println("Store file not found: "+e.getMessage());
+      }
+   }
+   private void encriptToFile(){
+      FileOutputStream fileOut = null;
+      try {
+         fileOut = new FileOutputStream("src/main/resources/enStore.txt");
+
+         ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+         for (Patient p: patientList){
+            objectOut.writeObject(p);
+         }
+         objectOut.close();
+
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
